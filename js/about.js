c0b5d4a218a22dc18abbb869bec4860a99a8b9eb
梁怀刚 <lianghuaigang@xiaoduotech.com>
2024-07-26 16:21:47 +0800

    Updates

--- js/about.js
+++ js/about.js
@@ -1,3 +1,5 @@
+initMobileMenu();
+
 AMapLoader.load({
   key: "d4b866e472685161d8e56754b2173469", //申请好的Web端开发者 Key，调用 load 时必填
   version: "2.0", //指定要加载的 JS API 的版本，缺省时默认为 1.4.15
@@ -6,10 +8,10 @@ AMapLoader.load({
     const position = [116.39, 39.9];
     const map = new AMap.Map("mapBox");
     const infoWindow = new AMap.InfoWindow({
-      position,
+      //   position,
       //创建信息窗体
       //   isCustom: true, //使用自定义窗体
-      content: `<div class="bg-white p-4">
+      content: `<div class="bg-white p-2 text-sm text-nowrap lg:p-4">
         <ul>
             <li>地址：北京市朝阳区阜通东大街6号院3号楼</li>
             <li>电话：12312313xxx</li>
@@ -21,7 +23,7 @@ AMapLoader.load({
       infoWindow.open(map, e.target.getPosition());
     };
 
-    infoWindow.open(map);
+    // infoWindow.open(map);
 
     const marker = new AMap.Marker({
       position,
