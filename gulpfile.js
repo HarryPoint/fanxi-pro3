6fb07d3264864d511e7054df64a42b78fd1ec50e
梁怀刚 <lianghuaigang@xiaoduotech.com>
2024-07-27 00:59:21 +0800

    feat: 生成文件

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,28 @@ const img = () => {
   console.log('----')
   return src("origin/*.{jpg,png}")
   .pipe(sharpResponsive({
-    formats: [
-      { 
-        width: (metadata) => {
-          return Math.floor(metadata.width * 0.5)
-        },
-        rename: { suffix: "-2x" }
-      }, {
-        rename: {suffix: '-1x'}
+    formats: [32, 90, 180, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048].map(width => {
+      return {
+        width,
+        rename: {suffix: `-${width}`}
       }
-    ]
+    })
+    // formats: [
+      // { 
+      //   width: (metadata) => {
+      //     return Math.floor(metadata.width * 0.3)
+      //   },
+      //   rename: { suffix: "-1x" }
+      // },
+      // { 
+      //   width: (metadata) => {
+      //     return Math.floor(metadata.width * 0.5)
+      //   },
+      //   rename: { suffix: "-2x" }
+      // }, {
+      //   rename: {suffix: '-3x'}
+      // }
+    // ]
   }))
   .pipe(dest("src/assets"))
 };
