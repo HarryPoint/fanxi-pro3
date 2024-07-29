00a2d99d7ea3a7d54ee3332bcef028147e5e40a1
梁怀刚 <lianghuaigang@xiaoduotech.com>
2024-07-29 17:58:54 +0800

    feat: 添加暂停功能

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -12,20 +12,26 @@ class Swiper {
 
     this.$el = document.querySelector(this.el);
     this.$wrapper = this.$el.querySelector(".swiper-wrapper");
-    this.$slides = Array.from(this.$wrapper.querySelectorAll(".swiper-slide")).map(item => {
-      const slide = document.createElement('div')
-      slide.classList.add('swiper-slide-wrapper', 'group', 'w-full', 'h-full', 'absolute');
+    this.$slides = Array.from(
+      this.$wrapper.querySelectorAll(".swiper-slide"),
+    ).map((item) => {
+      const slide = document.createElement("div");
+      slide.classList.add(
+        "swiper-slide-wrapper",
+        "group",
+        "w-full",
+        "h-full",
+        "absolute",
+      );
       slide.append(item);
-      return slide
+      return slide;
     });
     this.$slides.forEach(($slide) => {
       $slide.addEventListener("mouseenter", () => {
         this.stop();
       });
       $slide.addEventListener("mouseleave", () => {
-        if (this.autoPlayFlag) {
-          this.autoPlay();
-        }
+        this.play();
       });
     });
 
@@ -35,7 +41,13 @@ class Swiper {
 
     // dom 构造
     this.$inner = document.createElement("div");
-    this.$inner.classList.add("swiper-wrapper-inner", "relative", "w-full", "h-full", "overflow-hidden");
+    this.$inner.classList.add(
+      "swiper-wrapper-inner",
+      "relative",
+      "w-full",
+      "h-full",
+      "overflow-hidden",
+    );
     this.$inner.append(...this.$slides);
     this.$wrapper.append(this.$inner);
 
@@ -43,25 +55,27 @@ class Swiper {
 
     this.$dots = Array.from({ length: this.slidesCount }).map((_, index) => {
       const dot = document.createElement("div");
-      dot.innerHTML = `<div class="inner"></div>`
-      dot.classList.add(...this.params.pagination.class, 'spin')
+      dot.innerHTML = `<div class="inner"></div>`;
+      dot.classList.add(...this.params.pagination.class, "spin");
       dot.style.cursor = "pointer";
       dot.addEventListener("click", () => {
         this.moveTo(index);
       });
+      dot.addEventListener("animationend", () => {
+        this.next();
+      });
       return dot;
     });
     this.$pagination.append(...this.$dots);
-    this.slideSetIn(this.$slides[this.currentIndex]);
-    this.dotSetIn(this.$dots[this.currentIndex]);
+    this.autoPlay();
   }
   slideSetOut(dom) {
-    dom.classList.remove('active', 'in');
-    dom.classList.add('out');
+    dom.classList.remove("active", "in");
+    dom.classList.add("out");
   }
   slideSetIn(dom) {
-    dom.classList.remove('out');
-    dom.classList.add('in', 'active');
+    dom.classList.remove("out");
+    dom.classList.add("in", "active");
   }
   dotSetOut(dot) {
     dot.classList.remove(...this.params.pagination.activeClass);
@@ -77,8 +91,8 @@ class Swiper {
         this.dotSetOut(dot);
       }
     });
-    this.slideSetOut(this.$slides[this.currentIndex])
-    this.slideSetIn(this.$slides[nextIndex])
+    this.slideSetOut(this.$slides[this.currentIndex]);
+    this.slideSetIn(this.$slides[nextIndex]);
     this.currentIndex = nextIndex;
   }
   next() {
@@ -96,19 +110,19 @@ class Swiper {
     }
   }
   autoPlay(tick = 5000) {
-    this.$dots.forEach(itm => {
-      itm.style.animationDuration = `${tick}ms`
-    })
-    this.autoPlayFlag = true;
-    this.timer = setInterval(() => {
-      this.next();
-    }, tick);
-    return () => {
-      this.stop();
-      this.autoPlayFlag = false;
-    };
+    this.$dots.forEach((itm) => {
+      itm.style.animationDuration = `${tick}ms`;
+    });
+    this.moveTo(this.currentIndex);
   }
   stop() {
-    clearInterval(this.timer);
+    this.$dots.forEach((itm) => {
+      itm.classList.add("stop");
+    });
+  }
+  play() {
+    this.$dots.forEach((itm) => {
+      itm.classList.remove("stop");
+    });
   }
 }
